cc299764d625f05add57fb66af5326d812c293ac
Will Crichton <will@MacBook-Pro.local>
2017-06-20 12:51:36 -0500

    Added flags to deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-rsync -avz --delete _site/ jemmons@ai.stanford.edu:/afs/cs.stanford.edu/group/gfx/www/content/visionsystems/
+USER=${1:-jemmons}
+SERVER=${2:-ai}
 
+rsync -avz --delete _site/ $USER@$SERVER.stanford.edu:/afs/cs.stanford.edu/group/gfx/www/content/visionsystems/
